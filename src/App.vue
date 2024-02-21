fbe3d6ccf6f4c810de40f6068ee31bcbd445bab3
gsy <2321756533@qq.com>
2024-02-21 16:38:40 +0800

    doc: 新增代码link, 组件回调

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
     <div id="app">
+        <div style="color: #409EFF; display: flex;justify-content: flex-end;align-items: center;padding: 10px 20px 0 0;cursor: pointer;"
+            @click="goCode">
+            <svg xmlns="http://www.w3.org/2000/svg" width="28px" height="22.5px" viewBox="0 0 640 512">
+                <path fill="currentColor"
+                    d="M392.8 1.2c-17-4.9-34.7 5-39.6 22l-128 448c-4.9 17 5 34.7 22 39.6s34.7-5 39.6-22l128-448c4.9-17-5-34.7-22-39.6zm80.6 120.1c-12.5 12.5-12.5 32.8 0 45.3L562.7 256l-89.4 89.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l112-112c12.5-12.5 12.5-32.8 0-45.3l-112-112c-12.5-12.5-32.8-12.5-45.3 0zm-306.7 0c-12.5-12.5-32.8-12.5-45.3 0l-112 112c-12.5 12.5-12.5 32.8 0 45.3l112 112c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256l89.4-89.4c12.5-12.5 12.5-32.8 0-45.3z" />
+            </svg>
+        </div>
+
         <el-card style="margin: 20px" header="AppTable：基础Table包含分页器">
             <AppTable :columns="tableQuery" :data="tableData" :pagination.sync="pagination"
                 @paginationChange="getTableData"></AppTable>
@@ -8,21 +16,11 @@
             <AppForm :columns="formColumns"></AppForm>
         </el-card>
         <el-card style="margin: 20px" header="AppCU：基础编辑和新增的校验">
-            <el-button type="primary" @click="
-                formColumns.clearValue()
-            cuOptions.title = '新增'
-            cuShow = true
+            <el-button type="primary" @click="setAddContent
                 ">
                 <i class="el-icon-plus"></i> 新增
             </el-button>
-            <el-button @click="
-                formColumns.setArrValue({
-                    test1: '测试1',
-                    test2: '1'
-                })
-            cuOptions.title = '编辑'
-            cuShow = true
-                ">
+            <el-button @click="setEditValue">
                 <i class="el-icon-edit"></i> 编辑
             </el-button>
         </el-card>
@@ -44,16 +42,32 @@ let cloneOption = new CloneOption([
         key: "test2",
         type: "select",
         options: [{ label: "测试1", value: "1" }],
-        rules: Rule.required("请输入测试字段")
+        rules: Rule.required("请输入测试字段"),
+    },
+    {
+        label: "测试字段3",
+        key: "test3",
+        type: "radio",
+        rules: Rule.required("请输入测试字段"),
+        options: [{ label: "测试radio1", value: "1" }, { label: "测试radio2", value: "2" }],
+        render: function (v, index, _) {
+            console.log(v, index, _) // v: 当前值 index: 当前索引 _: 当前字段配置
+            let keyString = _.key
+            let opts = _.options
+            return opts.find((item) => item.value === v[keyString]).label
+        }
     }
+    // 可以尝试在这新增部分字段
 ])
+// 表单数据 - 对cloneOption进行处理(移除不用字段)
 let tableQuery = cloneOption
     .clone()
     .getAll()
     .map((item) => ({
         label: item.label,
         key: item.key,
-        render: item.render || null
+        render: item.render || null,
+        options: item.options || null
     }))
 export default {
     name: "App",
@@ -62,27 +76,19 @@ export default {
         AppForm,
         AppCU
     },
-    created() { },
     data() {
         return {
             tableQuery,
             formColumns: cloneOption.clone(),
-            tableData: [
-                {
-                    test1: "测试1",
-                    test2: "测试2"
-                }
-            ],
+            // 模拟表单数据
+            tableData: [{ test1: "测试1", test2: "测试2", test3: "2" }],
             pagination: {
                 currentPage: 1,
                 pageSize: 10,
                 total: 1
             },
             cuShow: false,
-            cuOptions: {
-                title: "新增",
-                width: "50%"
-            },
+            cuOptions: { title: "新增", width: "50%" },
             cuColumns: cloneOption.clone()
         }
     },
@@ -104,10 +110,25 @@ export default {
             ]
             this.pagination.total = 200
         },
+        setAddContent() {
+            // 模拟新增数据
+            this.formColumns.setArrValue({ test1: '测试1', test2: '1' })
+            this.cuOptions.title = '新增'
+            this.cuShow = true
+        },
+        setEditValue() {
+            // 模拟新增数据
+            this.formColumns.setArrValue({ test1: '测试1', test2: '1' })
+            this.cuOptions.title = '编辑'
+            this.cuShow = true
+        },
         confirmCU(data) {
-            console.log("confirmCU", data)
+            console.log('提交数据', data)
             // TODO: 提交数据
             this.cuShow = false
+        },
+        goCode() {
+            window.open('https://github.com/guoshuangyang/v2-ele-curd/blob/main/src/App.vue', '_blank')
         }
     }
 }
--- src/components/curd/AppTable.vue
+++ src/components/curd/AppTable.vue
@@ -1,197 +1,166 @@
 <template>
-  <div class="flex flex-wrap justify-center app-table">
-    <div class="w-full">
-      <el-table
-        v-bind="$attrs"
-        width="100%"
-        :header-cell-style="{
-          background: '#F6F8FC',
-          height: '40px',
-          fontWeight: '600',
-          color: '#333',
-          border: 0
-        }"
-        border
-        stripe
-        header-cell-class-name="element_plus_app_table_header"
-        v-loading="loading"
-        v-on="$listeners"
-      >
-        <el-table-column v-if="hasSelect" type="selection" width="55" />
-        <template v-for="item in showColumns">
-          <slot :name="item.key">
-            <el-table-column
-              :label="item.label"
-              :key="item.key"
-              :prop="item.key"
-              :width="item.width || undefined"
-              :align="align"
-            >
-              <template v-if="item.render" #default="scope">
-                {{ item.render(scope.row, scope.$index) }}
-              </template>
-            </el-table-column>
-          </slot>
-        </template>
-        <slot name="default"> </slot>
-      </el-table>
+    <div class="flex flex-wrap justify-center app-table">
+        <div class="w-full">
+            <el-table v-bind="$attrs" width="100%" :header-cell-style="{
+                background: '#F6F8FC',
+                height: '40px',
+                fontWeight: '600',
+                color: '#333',
+                border: 0
+            }" border stripe header-cell-class-name="element_plus_app_table_header" v-loading="loading" v-on="$listeners">
+                <el-table-column v-if="hasSelect" type="selection" width="55" />
+                <template v-for="item in showColumns">
+                    <slot :name="item.key">
+                        <el-table-column :label="item.label" :key="item.key" :prop="item.key"
+                            :width="item.width || undefined" :align="align">
+                            <template v-if="item.render" #default="scope">
+                                {{ item.render(scope.row, scope.$index, item) }}
+                            </template>
+                        </el-table-column>
+                    </slot>
+                </template>
+                <slot name="default"> </slot>
+            </el-table>
+        </div>
+        <el-pagination class="app-table-pagination" :current-page.sync="paginationObj.currentPage"
+            :page-size.sync="paginationObj.pageSize" :page-sizes="[10, 20, 50, 100]" background
+            layout="sizes, prev, pager, next, total" v-if="hasPagination" :total="paginationObj.total"
+            @size-change="handleSizeChange" @current-change="handleCurrentChange" />
+        <el-drawer :visible.sync="filterDrawer" size="400px" title="表格显示列" :before-close="handleClose" append-to-body>
+            <div class="px-5">
+                <el-checkbox v-model="checkAll" :indeterminate="isIndeterminate"
+                    @change="handleCheckAllChange">全选</el-checkbox>
+                <el-checkbox-group v-model="checkedItems">
+                    <el-checkbox v-for="item in realColumns" :key="item.key" :label="item.key">{{
+                        item.label
+                    }}</el-checkbox>
+                </el-checkbox-group>
+            </div>
+        </el-drawer>
     </div>
-    <el-pagination
-      class="app-table-pagination"
-      :current-page.sync="paginationObj.currentPage"
-      :page-size.sync="paginationObj.pageSize"
-      :page-sizes="[10, 20, 50, 100]"
-      background
-      layout="sizes, prev, pager, next, total"
-      v-if="hasPagination"
-      :total="paginationObj.total"
-      @size-change="handleSizeChange"
-      @current-change="handleCurrentChange"
-    />
-    <el-drawer
-      :visible.sync="filterDrawer"
-      size="400px"
-      title="表格显示列"
-      :before-close="handleClose"
-      append-to-body
-    >
-      <div class="px-5">
-        <el-checkbox
-          v-model="checkAll"
-          :indeterminate="isIndeterminate"
-          @change="handleCheckAllChange"
-          >全选</el-checkbox
-        >
-        <el-checkbox-group v-model="checkedItems">
-          <el-checkbox v-for="item in realColumns" :key="item.key" :label="item.key">{{
-            item.label
-          }}</el-checkbox>
-        </el-checkbox-group>
-      </div>
-    </el-drawer>
-  </div>
 </template>
 
 <script>
 export default {
-  name: "AppTable",
-  props: {
-    columns: {
-      type: Array,
-      default: () => []
-    },
-    // 要隐藏的列，根据key来隐藏
-    hiddenColumns: {
-      type: Array,
-      default: () => []
-    },
-    hasSelect: {
-      type: Boolean,
-      default: false
-    },
-    loading: {
-      type: Boolean,
-      default: false
-    },
-    pagination: {
-      type: Object,
-      default: () => ({
-        currentPage: 1,
-        pageSize: 10,
-        total: 0
-      })
-    },
-    align: {
-      type: String,
-      default: "left"
-    },
-    hasPagination: {
-      type: Boolean,
-      default: true
-    }
-  },
-  computed: {
-    realColumns() {
-      return this.columns.filter((item) => !item.tableHidden)
-    },
-    paginationObj: {
-      get() {
-        return this.pagination
-      },
-      set(val) {
-        this.$emit("update:pagination", val)
-      }
-    },
-    isIndeterminate() {
-      return this.checkedItems.length > 0 && this.checkedItems.length < this.realColumns.length
-    }
-  },
-  data() {
-    return {
-      filterDrawer: false,
-      checkAll: true,
-      showColumns: [],
-      checkedItems: []
-    }
-  },
-  created() {
-    this.checkedItems = this.realColumns.map((item) => item.key)
-  },
-  watch: {
-    hiddenColumns: {
-      immediate: true,
-      handler(val) {
-        let arr = []
-        this.realColumns.forEach((item) => {
-          if (!val.includes(item.key)) {
-            arr.push(item)
-          }
-        })
-        this.showColumns = arr
-      }
-    }
-  },
-  methods: {
-    handleClose(done) {
-      this.filterDrawer = false
-      let hiddenArr = []
-      this.realColumns.forEach((item) => {
-        if (!this.checkedItems.includes(item.key)) {
-          hiddenArr.push(item.key)
+    name: "AppTable",
+    props: {
+        columns: {
+            type: Array,
+            default: () => []
+        },
+        // 要隐藏的列，根据key来隐藏
+        hiddenColumns: {
+            type: Array,
+            default: () => []
+        },
+        hasSelect: {
+            type: Boolean,
+            default: false
+        },
+        loading: {
+            type: Boolean,
+            default: false
+        },
+        pagination: {
+            type: Object,
+            default: () => ({
+                currentPage: 1,
+                pageSize: 10,
+                total: 0
+            })
+        },
+        align: {
+            type: String,
+            default: "left"
+        },
+        hasPagination: {
+            type: Boolean,
+            default: true
         }
-      })
-      this.$emit("hiddenColumnsChange", hiddenArr)
-      this.showColumns = this.realColumns.filter((item) => this.checkedItems.includes(item.key))
-      done()
     },
-    handleSizeChange(val) {
-      this.paginationObj.pageSize = val
-      this.paginationObj.currentPage = 1
-      this.$emit("paginationChange", this.paginationObj)
+    computed: {
+        realColumns() {
+            return this.columns.filter((item) => !item.tableHidden)
+        },
+        paginationObj: {
+            get() {
+                return this.pagination
+            },
+            set(val) {
+                this.$emit("update:pagination", val)
+            }
+        },
+        isIndeterminate() {
+            return this.checkedItems.length > 0 && this.checkedItems.length < this.realColumns.length
+        }
     },
-    handleCurrentChange(val) {
-      this.paginationObj.currentPage = val
-      this.$emit("paginationChange", this.paginationObj)
+    data() {
+        return {
+            filterDrawer: false,
+            checkAll: true,
+            showColumns: [],
+            checkedItems: []
+        }
     },
-    handleCheckAllChange(val) {
-      this.checkedItems = val ? this.realColumns.map((item) => item.key) : []
+    created() {
+        this.checkedItems = this.realColumns.map((item) => item.key)
     },
-    showFilter() {
-      this.filterDrawer = true
+    watch: {
+        hiddenColumns: {
+            immediate: true,
+            handler(val) {
+                let arr = []
+                this.realColumns.forEach((item) => {
+                    if (!val.includes(item.key)) {
+                        arr.push(item)
+                    }
+                })
+                this.showColumns = arr
+            }
+        }
+    },
+    methods: {
+        handleClose(done) {
+            this.filterDrawer = false
+            let hiddenArr = []
+            this.realColumns.forEach((item) => {
+                if (!this.checkedItems.includes(item.key)) {
+                    hiddenArr.push(item.key)
+                }
+            })
+            this.$emit("hiddenColumnsChange", hiddenArr)
+            this.showColumns = this.realColumns.filter((item) => this.checkedItems.includes(item.key))
+            done()
+        },
+        handleSizeChange(val) {
+            this.paginationObj.pageSize = val
+            this.paginationObj.currentPage = 1
+            this.$emit("paginationChange", this.paginationObj)
+        },
+        handleCurrentChange(val) {
+            this.paginationObj.currentPage = val
+            this.$emit("paginationChange", this.paginationObj)
+        },
+        handleCheckAllChange(val) {
+            this.checkedItems = val ? this.realColumns.map((item) => item.key) : []
+        },
+        showFilter() {
+            this.filterDrawer = true
+        }
     }
-  }
 }
 </script>
 
 <style lang="scss">
 .app-table {
-  .app-table-pagination {
-    margin-top: 10px;
-    text-align: right;
-  }
-  .el-checkbox {
-    margin-right: 10px;
-    margin-bottom: 10px;
-  }
+    .app-table-pagination {
+        margin-top: 10px;
+        text-align: right;
+    }
+
+    .el-checkbox {
+        margin-right: 10px;
+        margin-bottom: 10px;
+    }
 }
 </style>
